40e83779db1d03e16f5273e27be54739a706d706
Desmond Madden <desmond.madden@hotmail.co.uk>
2021-12-20 23:43:18 +0000

    Fixing unit tests

--- tests/test_tweet.py
+++ tests/test_tweet.py
@@ -1,8 +1,5 @@
 from main.tweet import Tweeter
-from unittest.mock import patch
 
-@patch('Tweeter.get_tweet', return_value='ABC')
-def test_return_true_if_tweet_is_correct_length(self):
+def test_return_true_if_tweet_is_correct_length():
     classUnder = Tweeter()
-    assert classUnder.should_post_tweet('ACV') == True
-    assert Tweeter().should_post_tweet('ABC') == True
+    assert classUnder.should_post_tweet('ABC') == True
